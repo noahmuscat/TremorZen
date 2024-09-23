b452c7811f3054c9fd167e1d96979f1e543ed5f5
Noah Muscat <noahmu@umich.edu>
2024-09-22 23:08:21 -0400

    Fixed graph

--- tremors.m
+++ tremors.m
@@ -1,5 +1,5 @@
 %% TremorZen Movement Quantification
-% this code uses .csv files created using tremorActivity.py in order to
+% This code uses .csv files created using tremorActivity.py in order to
 % analyze the movement of a tremor patient's hand
 
 tremor_data = readtable("withtremor_actigraphy.csv");
@@ -8,10 +8,13 @@ notremor_data = readtable("withouttremor_actigraphy.csv");
 meanActivityTremor = mean(tremor_data.SelectedPixelDifference);
 meanActivityNoTremor = mean(notremor_data.SelectedPixelDifference);
 
-x_vals = [1, 2];
 means = [meanActivityNoTremor, meanActivityTremor];
 
 figure;
-b1 = bar(x_vals, means, 'BarWidth', 1);
+b1 = bar(means, 'BarWidth', 1);
 title('Tremor vs. Non-Tremor Hand Movement');
+xticks([1 2])
+xticklabels({'No Tremor', 'Tremor'})
+xlabel('Condition');
+ylabel('Hand Movement');
 
